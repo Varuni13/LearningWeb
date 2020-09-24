24a075b207feb086f59a31bdbd8fcf35cfe5fd58
Varuni13 <varunisingh131096@gmail.com>
2020-09-24 22:41:13 +0530

    Initialized

---
+++ learn-to-code.js
@@ -0,0 +1,15 @@
+
+    // var name=" Varuni";
+    // var age = 24;
+    // var message = "Hi, my name is " + name + " and I am " + age + " years old!";
+    // //alert(message);
+
+    var firstName = "Varuni";
+    var lastName = "Singh";
+    var dateOfBirth = "13-10-1996";
+    var age = 23;
+    var profileImgUrl = 'http://coolpicks.com/johnjacob.jpg';
+     
+    var loginWelcomeMessage = "Welcome, " + firstName + ". Happy " + age + "rd birthday!";
+
+    console.log(loginWelcomeMessage); 
